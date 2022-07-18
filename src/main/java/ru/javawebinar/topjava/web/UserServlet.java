50ea899d2c5a7b2e822bd132b4dfda65787fabe4
Fulkin <sea-xv@mail.ru>
2022-07-18 14:46:29 +0500

    Apply 2.3 app layers

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.*;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
